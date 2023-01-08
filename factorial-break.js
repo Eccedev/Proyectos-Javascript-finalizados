86f02d7984e1d591f514cbe65f147fd5c92b4efd
eccedev <117386916+Eccedev@users.noreply.github.com>
2023-01-08 12:09:06 +0000

    Add files via upload

---
+++ factorial-break.js
@@ -0,0 +1,8 @@
+let i = 0;
+while (i <= 10) {
+    i++;
+    if (i > 10) {
+        break;
+    }
+    console.log(i);
+}
---
+++ factorial-while.js
@@ -0,0 +1,6 @@
+let i = 0;
+let b = 10;
+while (i <= b) {
+    i++
+    console.log(i)
+    }
