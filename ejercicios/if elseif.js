95ae104122f1dde610a2502d23460faea237c546
eccedev <117386916+Eccedev@users.noreply.github.com>
2023-02-24 14:22:23 +0000

    Create if elseif.js

---
+++ ejercicios/if elseif.js
@@ -0,0 +1,16 @@
+let num = 6;
+if (num % 2 === 0) {
+  console.log("it is even");    // "it is even"
+} else {
+  console.log("it is odd");
+}
+
+---------
+let num1 = 3;
+let num2 = 5;
+if (num1 > num2) {
+  console.log("num1 bigger");
+};
+else if (num2 > num1) {
+  console.log("num2 bigger");  // "num2 bigger"
+};
