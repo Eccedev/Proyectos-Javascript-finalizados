913736c039787e1f169372423f958dab6bdd2208
eccedev <117386916+Eccedev@users.noreply.github.com>
2023-08-31 19:37:30 +0100

    Add files via upload

---
+++ PROYECTOS FINALIZADOS/citas aleatorias/citas.js
@@ -0,0 +1,70 @@
+let citas = [
+    {
+      'autor': 'Diógenes de Sínope',
+      'texto': 'Gente mucha, personas pocas.'
+    },
+    {
+      'autor': 'Albert Camus',
+      'texto': 'La estupidez insiste siempre.'
+    },
+    {
+      'autor': 'Víctor Hugo',
+      'texto': 'La risa es el sol que ahuyenta el invierno del rostro humano.'
+    },
+    {
+      'autor': 'Sócrates',
+      'texto': 'Habla para que yo te conozca.'
+    },
+    {
+      'autor': 'Albert Einstein',
+      'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+    },
+    {
+      'autor': 'Anónimo',
+      'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+    },
+    {
+      'autor': 'Alan Kay',
+      'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+    },
+    {
+      'autor': 'Amelia Earhart',
+      'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+    },
+    {
+      'autor': 'Jacinto Benavente',
+      'texto': ' La disciplina consiste en que un imbécil se haga obedecer por los que son más inteligentes.'
+    },
+    {
+      'autor': 'Fernando Pessoa',
+      'texto': 'No siente la libertad quien nunca vivió oprimido.' 
+    },
+    {
+      'autor': 'Leonardo da Vinci',
+      'texto': 'La sencillez es la máxima sofisticación. ' 
+    },
+    {
+      'autor': 'Confucio',
+      'texto': 'No importa lo lento que vayas mientras no te detengas.' 
+    },
+    {
+      'autor': 'Jacinto Benavente',
+      'texto': 'Cuando no se piensa lo que se dice es cuando se dice lo que se piensa.'
+    },
+    {
+      'autor': 'Charles R. Swindoll',
+      'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+    },
+    {
+      'autor': 'Jane Goodall',
+      'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+    },
+    {
+      'autor': 'John Muir',
+      'texto': 'El poder de la imaginación nos hace infinitos.'
+    },
+    {
+      'autor': 'Mark Twain',
+      'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+    }
+  ];
